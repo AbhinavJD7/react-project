7e140b0c9c0e589b2131c179a1944fe1eb59a280
Abhinav Rai <122885696+AbhinavJD7@users.noreply.github.com>
2025-06-29 03:15:46 +0530

    Understood Hooks by counter App

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(5)
+
+  //let counter = 5
+
+
+  const addValue = () => {
+    console.log("Clicked" , counter);
+    //counter = counter + 1;
+    if(counter<=19)
+    setCounter(counter + 1)
+  }
+
+   const removeValue = () => {
+    console.log("Clicked" , counter);
+    //counter = counter + 1;
+    if(counter>0)
+    setCounter(counter - 1)
+  }
+
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <h1>Counter Value: {counter}</h1>
+
+      <button onClick={addValue}>Add Value</button>
+      <br/>
+      <button onClick={removeValue}>Remove Value</button>
+
+    </>
+  )
+}
+
+export default App
